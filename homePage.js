77d09c960498ddfab9362626181f3ce3973db037
hilaBenDavid <hila9il@gmail.com>
2023-09-12 09:15:52 +0300

    fgjfu

---
+++ homePage.js
@@ -0,0 +1,15 @@
+const carousel = document.querySelector(".carousel");
+carousel.addEventListener("click", () => {console.log("carousel")});
+
+const arrowNext = document.querySelector(".next-btn");
+arrowNext.addEventListener("click", () => {console.log("next")});
+
+const arrowPrev = document.querySelector(".prev-btn");
+arrowPrev.addEventListener("click", () => {console.log("previous")});
+
+const 
+// const dragging = (e) => {
+//   carousel.scrollLeft = e.pageX;
+// }
+
+// carousel.addEventListener("mousemove", dragging);
