5ac195fa2d4908c48758ddcce1d5a844cd5753b4
hilaBenDavid <hila9il@gmail.com>
2023-10-25 23:44:29 +0300

    trying to fix jtbd

--- homePage.js
+++ homePage.js
@@ -1,9 +1,11 @@
 function leftScroll(){
+    console.log("got into left scroll function");
     const left = document.querySelector(".scroller");
     left.scrollBy(-400,0);
 }
 
 function rightScroll(){
+    console.log("got into right scroll function");
     const right = document.querySelector(".scroller");
     right.scrollBy(400,0);
 }
